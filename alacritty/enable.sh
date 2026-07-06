333eebbb8db5f15d672064715454d76637dbbc99
Coffee <35109746+coffeeisangry@users.noreply.github.com>
2026-07-06 21:19:38 +0100

    Fix alacritty enable.sh

--- alacritty/enable.sh
+++ alacritty/enable.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 ALACRITTY_CONFIG="$HOME/.config/alacritty/alacritty.toml"
-IMPORT_LINE='import = ["~/.config/alacritty/colors.toml"]'
+COLORS_PATH="~/.config/alacritty/colors.toml"
+IMPORT_BLOCK=$'[general]\nimport = ["'"$COLORS_PATH"'"]'
 
 # create config if it doesn't exist
 if [ ! -f "$ALACRITTY_CONFIG" ]; then
     mkdir -p "$(dirname "$ALACRITTY_CONFIG")"
-    echo "$IMPORT_LINE" > "$ALACRITTY_CONFIG"
-    echo "Created $ALACRITTY_CONFIG with import line"
+    echo "$IMPORT_BLOCK" > "$ALACRITTY_CONFIG"
+    echo "Created $ALACRITTY_CONFIG with import block"
     exit 0
 fi
 
@@ -18,5 +18,5 @@ if grep -qF 'colors.toml' "$ALACRITTY_CONFIG"; then
 fi
 
 # prepend to the top of the file so it's picked up first
-echo -e "$IMPORT_LINE\n$(cat "$ALACRITTY_CONFIG")" > "$ALACRITTY_CONFIG"
-echo "Added import line to $ALACRITTY_CONFIG"
+echo -e "$IMPORT_BLOCK\n\n$(cat "$ALACRITTY_CONFIG")" > "$ALACRITTY_CONFIG"
+echo "Added import block to $ALACRITTY_CONFIG"
